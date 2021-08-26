d6e1f5892e4bcd5930170aab6dcf2857d782b0dd
Juan Esteban Marin <62470801+imaes11st@users.noreply.github.com>
2021-08-26 17:45:10 -0500

    Formulario para listar personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de personas';
+  personas: Persona[] = [new Persona('Juan','Marin'), new Persona('Sofia','Torres'), new Persona('Alejandra', 'Rincon')];
+
+  nombreInput: string='';
+  apellidoInput: string='';
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
+
 }
